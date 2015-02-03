cdd5f65765273fdaafd47c167a154ad99729bdf3
caffeinatedDrummer <hyprvx@gmail.com>
2015-02-03 17:28:53 -0600

    Condensed, curved.
    I learned you can set the RobotDrive class to use 4 motor controllers
    instead of the 2 sets of 2 we were using. Renamed our RobotDrive object
    to just "Drive", removed all things to Tier2, set Drive to use 4
    controllers.
    
    I also learned that to make a squared input (Motor curve, exponential),
    you just add a boolean statement to the end of your arcadeDrive
    instruction. So we have leftSide, rightSide, and true in our arcadeDrive
    instructions to add a motor curve. Removed the IF/THEN logic at the
    start of the program since it's now been implemented.

--- src/org/usfirst/frc/team2151/robot/Robot.java
+++ src/org/usfirst/frc/team2151/robot/Robot.java
@@ -9,10 +9,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DigitalInput;
 //to change the motor power: find gamePad.getRawAxis and edit the multiplying value.
 //do this for all getRawAxis values. Be consistent!
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
-    RobotDrive Tier1;//The first set of Talons.
-    RobotDrive Tier2;//The second set of Talons.
+    RobotDrive Drive;//The first set of Talons.
     Joystick gamePad; //our gamepad! wooo.
     Relay relayArms; //our arms lifting mech
     Relay relayGrab; //the grabby bit
@@ -20,10 +20,8 @@ public class Robot extends SampleRobot {
     DigitalInput limitSwitch2;
     
     public Robot() {
-        Tier1 = new RobotDrive(0, 2);
-        Tier1.setExpiration(0.1);
-        Tier2 = new RobotDrive(1, 3);
-        Tier2.setExpiration(0.1);
+        Drive = new RobotDrive(0, 1, 2, 3);
+        Drive.setExpiration(0.1);
         relayArms = new Relay(0);
         gamePad = new Joystick(0); //init code
         limitSwitch1 = new DigitalInput(0);
@@ -34,34 +32,19 @@ public class Robot extends SampleRobot {
 
     
       public void operatorControl() {
-        Tier1.setSafetyEnabled(true);
-        Tier2.setSafetyEnabled(true);
+        Drive.setSafetyEnabled(true);
+        
         while (isOperatorControl() && isEnabled()) {
         	boolean buttonLower = gamePad.getRawButton(6); //for lowering the arms
         	boolean buttonRaise = gamePad.getRawButton(5); //or raising them    	
         	double leftSide = gamePad.getRawAxis(1) * .5; //50% power
         	double rightSide = gamePad.getRawAxis(2) * .5; //otherwise we move insanely too fast
-            	
-        	/*
-        	if (leftSide < 0)
-            	leftSide = leftSide * leftSide * -1; //Exponential increases, we need If/Then logic here
-            else
-           		leftSide = leftSide *leftSide;
-            if (rightSide < 0)
-           		rightSide = rightSide * rightSide * -1; //Multiply by -1 to make the value negative 
-            else            //if the input was negative, since squaring
-            	rightSide = rightSide * rightSide;      //a negative will always make a positive and
-        	
-        	Above is a motor curve logic bit. Uncomment if you wish to test it.
-        	*/
-        	
         	
         	while (buttonLower && limitSwitch2.get()) { //entering loops for raising and lowering arms
         		relayArms.set(Relay.Value.kForward);
         		leftSide = gamePad.getRawAxis(1) * .50;
             	rightSide = gamePad.getRawAxis(2) * .50;
-            	Tier1.arcadeDrive(-leftSide, -rightSide);
-            	Tier2.arcadeDrive(-leftSide, -rightSide);
+            	Drive.arcadeDrive(-leftSide, -rightSide, true);
             	buttonLower = gamePad.getRawButton(6);
             	Timer.delay(0.001);
         	}
@@ -70,8 +53,7 @@ public class Robot extends SampleRobot {
         		relayArms.set(Relay.Value.kReverse); //or raise them
             	leftSide = gamePad.getRawAxis(1) * .50; 
             	rightSide = gamePad.getRawAxis(2) * .50;
-            	Tier1.arcadeDrive(-leftSide, -rightSide); 
-            	Tier2.arcadeDrive(-leftSide, -rightSide);
+            	Drive.arcadeDrive(-leftSide, -rightSide, true); 
             	buttonRaise = gamePad.getRawButton(5); 
             	Timer.delay(0.001);
         	}
@@ -79,8 +61,7 @@ public class Robot extends SampleRobot {
         	
         	
         	relayArms.set(Relay.Value.kOff); //we were missing these commands and got very confused when the relays stuck on
-        	Tier1.arcadeDrive(-leftSide, -rightSide); //negated because it's backwards
-        	Tier2.arcadeDrive(-leftSide, -rightSide);
+        	Drive.arcadeDrive(-leftSide, -rightSide, true); //negated because it's backwards
             Timer.delay(0.001);		//1ms delay for very fast updating (now watch as we run out of memory)
         }
     }
