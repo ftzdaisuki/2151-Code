c6913f74c56ded3d57252edbe334f16371298796
Nate-Rod <nate-rod@live.com>
2015-02-05 16:16:58 -0600

    Added Victor Prototype Code
    Toying with the idea of using the Ultrasonic Range Finder to assist with
    TeleOp. Mapped buttons to open/close arms (see lines 49-53). Other
    stuff.

--- src/org/usfirst/frc/team2151/robot/Robot.java
+++ src/org/usfirst/frc/team2151/robot/Robot.java
@@ -18,7 +18,7 @@ public class Robot extends SampleRobot {
     RobotDrive Drive;//The first set of Talons.
     Joystick gamePad; //our gamepad! wooo.
     Relay relayArms; //opening and closing the arms
-    Victor armsLift; //lifting said arms
+    Victor armsLift; //the grabby bit
     DigitalInput limitSwitch1;
     DigitalInput limitSwitch2;
     Ultrasonic ultra;
@@ -30,8 +30,9 @@ public class Robot extends SampleRobot {
         armsLift = new Victor(4);
         gamePad = new Joystick(0); //init code
         limitSwitch1 = new DigitalInput(0);
-        limitSwitch2 = new DigitalInput(1);	
-        ultra = new Ultrasonic(3,4); //these ports are just used as placeholders. Change to (ULTRASONIC_ECHO_PULSE_OUTPUT, ULTRASONIC_TRIGGER_PULSE_INPUT)
+        limitSwitch2 = new DigitalInput(1);
+        ultra = new Ultrasonic(6,5); //these ports are just used as placeholders. 
+        //Change to (ULTRASONIC_ECHO_PULSE_OUTPUT, ULTRASONIC_TRIGGER_PULSE_INPUT)
         ultra.setEnabled(true);
         ultra.setAutomaticMode(true);
         
@@ -45,40 +46,42 @@ public class Robot extends SampleRobot {
         Drive.setSafetyEnabled(true);
         
         while (isOperatorControl() && isEnabled()) {
-        	boolean buttonClose = gamePad.getRawButton(6); //for lowering the arms
-        	boolean buttonOpen = gamePad.getRawButton(5); //or raising them    	
-        	double leftSide = gamePad.getRawAxis(1) * .60; //60% power
+        	boolean buttonLower = gamePad.getRawButton(6); //for lowering the arms
+        	boolean buttonRaise = gamePad.getRawButton(5); //or raising them  
+        	boolean armsClose = gamePad.getRawButton(8); //for closing the arms
+        	boolean armsOpen = gamePad.getRawButton(7); //for opening them up again
+        	double leftSide = gamePad.getRawAxis(1) * .60; //50% power
         	double rightSide = gamePad.getRawAxis(2) * .60; //otherwise we move insanely too fast
         	double range = ultra.getRangeInches();
-
         	
-        	while (buttonClose && limitSwitch2.get()) { //entering loops for raising and lowering arms
+        	/* toying around with the idea of using the buttons to open and close the arms
+        	   while (armsClose && range < 2) {
+        		
+        	}
+        	*/
+        	while (buttonLower && limitSwitch2.get()) { //entering loops for raising and lowering arms
         		relayArms.set(Relay.Value.kForward);
         		leftSide = gamePad.getRawAxis(1) * .60;
             	rightSide = gamePad.getRawAxis(2) * .60;
             	Drive.arcadeDrive(-leftSide, -rightSide, true);
-            	buttonClose = gamePad.getRawButton(6);
-            	Timer.delay(0.0001);
+            	buttonLower = gamePad.getRawButton(6);
+            	Timer.delay(0.001);
         	}
         	
-        	while (buttonOpen && limitSwitch1.get()) {
+        	while (buttonRaise && limitSwitch1.get()) {
         		relayArms.set(Relay.Value.kReverse); //or raise them
             	leftSide = gamePad.getRawAxis(1) * .60; 
             	rightSide = gamePad.getRawAxis(2) * .60;
             	Drive.arcadeDrive(-leftSide, -rightSide, true); 
-            	buttonOpen = gamePad.getRawButton(5); 
-            	Timer.delay(0.0001);
+            	buttonRaise = gamePad.getRawButton(5); 
+            	Timer.delay(0.001);
         	}
 			
         	
-        	SmartDashboard.putNumber("Ultrasonic range to nearest tote", range);
-        	SmartDashboard.putBoolean("Closing?", buttonClose);
-        	SmartDashboard.putBoolean("Opening?", buttonOpen);
-        	SmartDashboard.putBoolean("Limit 1", limitSwitch1.get());
-        	SmartDashboard.putBoolean("Limit 2", limitSwitch2.get());
+        	
         	relayArms.set(Relay.Value.kOff); //we were missing these commands and got very confused when the relays stuck on
         	Drive.arcadeDrive(-leftSide, -rightSide, true); //negated because it's backwards
-            Timer.delay(0.0001);		//0.1ms delay for very fast updating (now watch as our CPU usage spikes)
+            Timer.delay(0.001);		//1ms delay for very fast updating (now watch as we run out of memory)
         }
     }
 }
